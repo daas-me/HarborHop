431b5167cb9d2a0791c79f692cb6ec780abc82eb
daas-me <110253987+daas-me@users.noreply.github.com>
2025-10-01 21:09:45 +0800

    Restore project files and fix structure

--- HarborHop/harbor_mgmt/urls.py
+++ HarborHop/harbor_mgmt/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/login', views.user_login, name='login'),  # example
+    path('login', views.user_login, name='login'), 
 ]
